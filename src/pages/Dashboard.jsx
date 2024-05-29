a407243bf3df0e2da29f30656405f3bdc4836ee7
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-05-29 21:56:15 +0530

    added active projects and Colabs

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -67,59 +67,78 @@ const Dashboard = ({ className, user, projects }) => {
           <div className="text-sm text-slate-400">Web Companion</div>
         </div>
       </div>
-      <section className=" flex flex-col gap-4 rounded-lg bg-blue-600 p-4 text-slate-200">
-        <header className=" flex items-center gap-4 border-b border-slate-200 pb-4">
-          <img
-            src={user.profile_url_compress}
-            className=" aspect-square w-20 rounded-full bg-slate-200"
-          ></img>
-          <div>
-            <h2 className=" text-3xl font-bold text-white">
-              {user.first_name} {user.last_name}
-            </h2>
-            <p className=" ">{user.field}</p>
-          </div>
-          <FaChevronRight className=" text-3xl" />
-        </header>
-        <main>
-          <p className=" text-xl">{user.user_desc}</p>
-        </main>
-        <footer className=" grid gap-4 text-base font-bold text-white">
-          <header className=" flex items-center gap-4">
-            <FaUser className=" text-xl" />
-            <h2>Skills</h2>
+      <section className="grid grid-cols-3 gap-4">
+        <article className=" col-span-2 flex flex-col gap-4 rounded-lg bg-blue-600 p-4 text-slate-200">
+          <header className=" flex items-center gap-4 border-b border-slate-200 pb-4">
+            <img
+              src={user.profile_url_compress}
+              className=" aspect-square w-20 rounded-full bg-slate-200"
+            ></img>
+            <div>
+              <h2 className=" text-3xl font-bold text-white">
+                {user.first_name} {user.last_name}
+              </h2>
+              <p className=" ">{user.field}</p>
+            </div>
+            <FaChevronRight className=" text-3xl" />
           </header>
-          <main className=" flex gap-4 text-blue-600">
-            {user.skills.map((skill, index) => (
-              <article
-                key={index}
-                className=" rounded-lg bg-slate-200 p-4 py-2"
-              >
-                {skill}
-              </article>
-            ))}
-            {/* <article>HTML</article>
+          <main>
+            <p className=" text-xl">{user.user_desc}</p>
+          </main>
+          <footer className=" grid gap-4 text-base font-bold text-white">
+            <header className=" flex items-center gap-4">
+              <FaUser className=" text-xl" />
+              <h2>Skills</h2>
+            </header>
+            <main className=" flex gap-4 text-blue-600">
+              {user.skills.map((skill, index) => (
+                <article
+                  key={index}
+                  className=" rounded-lg bg-slate-200 p-4 py-2"
+                >
+                  {skill}
+                </article>
+              ))}
+              {/* <article>HTML</article>
             <article>HTML</article>
             <article>HTML</article> */}
-          </main>
-        </footer>
-        <footer className=" grid gap-4 text-base font-bold text-white">
-          <header className=" flex items-center gap-4">
-            <FaTable className=" text-xl" />
-            <h2>Projects</h2>
-          </header>
-          <main className=" flex gap-4 text-blue-600">
-            {user.projects.map((project, index) => (
-              <article
-                key={project.project_id}
-                className=" max-w-60 rounded-lg bg-slate-200 p-4 py-2"
-              >
-                <span>{project.project_name}</span>
-                <details>{project.project_desc}</details>
-              </article>
-            ))}
-          </main>
-        </footer>
+            </main>
+          </footer>
+          <footer className=" grid gap-4 text-base font-bold text-white">
+            <header className=" flex items-center gap-4">
+              <FaTable className=" text-xl" />
+              <h2>Projects</h2>
+            </header>
+            <main className=" flex gap-4 text-blue-600">
+              {user.projects.map((project, index) => (
+                <article
+                  key={project.project_id}
+                  className=" max-w-60 rounded-lg bg-slate-200 p-4 py-2"
+                >
+                  <span>{project.project_name}</span>
+                  <details>{project.project_desc}</details>
+                </article>
+              ))}
+            </main>
+          </footer>
+        </article>
+        <article className=" grid grid-rows-3 gap-4">
+          <div className=" flex items-center justify-between gap-4 rounded-lg bg-slate-200 p-4 text-3xl font-bold">
+            <span className="text-blue-600">Active Projects</span>
+            <div className=" flex aspect-square w-12 items-center justify-center rounded-full bg-blue-600 font-bold text-white ">
+              {projects.length}
+            </div>
+          </div>
+          <div className=" flex items-center justify-between gap-4 rounded-lg bg-slate-200 p-4 text-3xl font-bold">
+            <span className="text-blue-600">Colabs</span>
+            <div className=" flex aspect-square w-12 items-center justify-center rounded-full bg-blue-600 font-bold text-white ">
+              {projects.reduce(
+                (acc, curr) => acc + curr.colab_requests.length,
+                0,
+              )}
+            </div>
+          </div>
+        </article>
       </section>
       <section className=" flex flex-col gap-4 ">
         <header className="rounded-lg bg-blue-600 p-4">
